951a4e785e981ac1ec9f4713ad42b5520d59b3e4
christian <nickolaou@oakland.edu>
2018-03-12 23:42:51 -0400

    changed the package names and moved classes

--- src/regalbank/gui/CustomerInput.java
+++ src/regalbank/gui/CustomerInput.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package databasegui;
+package regalbank.gui;
 import java.sql.*;
 
 /**
--- src/regalbank/gui/DataBaseGUI.java
+++ src/regalbank/gui/DataBaseGUI.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package databasegui;
+package regalbank.gui;
 
 import java.sql.*;
 import java.awt.GridLayout;
