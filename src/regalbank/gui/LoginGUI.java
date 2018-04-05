ccba588be5761230c64146e9aee5fb40ac1867ba
Christian Nickolaou <nickolaou@oakland.edu>
2018-04-05 11:54:05 -0400

    added load data button to the login gui

--- src/regalbank/gui/LoginGUI.java
+++ src/regalbank/gui/LoginGUI.java
@@ -45,6 +45,7 @@ public class LoginGUI extends javax.swing.JFrame {
         PasswordField = new javax.swing.JTextField();
         PasswordLabel = new javax.swing.JLabel();
         LoginButton = new javax.swing.JButton();
+        loadDataButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -61,6 +62,8 @@ public class LoginGUI extends javax.swing.JFrame {
                 LoginButtonActionPerformed(evt);
             }
         });
+
+        loadDataButton.setText("Load Data");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -79,7 +82,9 @@ public class LoginGUI extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap()
+                        .addComponent(loadDataButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(LoginButton))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(12, 12, 12)
@@ -99,7 +104,9 @@ public class LoginGUI extends javax.swing.JFrame {
                     .addComponent(PasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(PasswordLabel))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
-                .addComponent(LoginButton)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LoginButton)
+                    .addComponent(loadDataButton))
                 .addContainerGap())
         );
 
@@ -198,6 +205,7 @@ public class LoginGUI extends javax.swing.JFrame {
     private javax.swing.JTextField PasswordField;
     private javax.swing.JLabel PasswordLabel;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton loadDataButton;
     // End of variables declaration//GEN-END:variables
 
 }
